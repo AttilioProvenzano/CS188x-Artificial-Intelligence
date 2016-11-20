b940ab710c6fbe20791ccec10d684a58c0836c6e
Attilio Provenzano <attilio.provenzano@gmail.com>
2016-11-20 17:50:18 +0000

    Completed Project1 (no extra credit)

--- Project1/search/searchAgents.py
+++ Project1/search/searchAgents.py
@@ -530,7 +530,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.uniformCostSearch(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -566,7 +566,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
