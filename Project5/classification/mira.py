50ede147e651ce8ab3772fed057d962558d8a7c2
Attilio Provenzano <attilio.provenzano@gmail.com>
2016-12-10 01:35:16 +0000

    Built a MIRA classifier

--- Project5/classification/mira.py
+++ Project5/classification/mira.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -60,8 +60,57 @@ class MiraClassifier:
         datum is a counter from features to values for those features
         representing a vector of values.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        bestAccuracy = None
+        bestWeights = None
+        bestC = None
+
+        startingWeights = self.weights
+
+        for C in Cgrid:
+            self.weights = startingWeights
+            for iteration in range(self.max_iterations):
+                print "Starting iteration ", iteration, "..."
+                for i in range(len(trainingData)):
+                    labelScores = [self.weights[label] * trainingData[i]
+                        for label in self.legalLabels]
+                    predictedLabelIndex = labelScores.index(max(labelScores))
+                    predictedLabel = self.legalLabels[predictedLabelIndex]
+
+                    if predictedLabel != trainingLabels[i]:
+                        w = self.weights[predictedLabel]
+                        wStar = self.weights[trainingLabels[i]]
+                        f = trainingData[i]
+
+                        tau = ((w - wStar) * f + 1.0) / (2 * (f * f))
+                        tau = min(C, tau)
+
+                        for j in f:
+                            w[j] -= tau * f[j]
+                            wStar[j] += tau * f[j]
+
+            # Evaluate accuracy for the current value of C
+            correctPredictions = 0
+            for i in range(len(validationData)):
+                labelScores = [self.weights[label] * validationData[i]
+                    for label in self.legalLabels]
+                predictedLabelIndex = labelScores.index(max(labelScores))
+                predictedLabel = self.legalLabels[predictedLabelIndex]
+
+                if predictedLabel == validationLabels[i]:
+                    correctPredictions += 1
+
+            accuracy = float(correctPredictions) / len(validationData)
+
+            if bestAccuracy is None or accuracy >= bestAccuracy:
+                # Favor lower values of C in tie-breaks
+                if accuracy == bestAccuracy and C > bestC:
+                    continue
+
+                bestC = C
+                bestAccuracy = accuracy
+                bestWeights = self.weights
+
+        self.weights = bestWeights
 
     def classify(self, data ):
         """
@@ -77,5 +126,3 @@ class MiraClassifier:
                 vectors[l] = self.weights[l] * datum
             guesses.append(vectors.argMax())
         return guesses
-
-
